c2d560d3fcb34509914054e0724dce05a5439e3e
Oleg Kiryushin <fewgwer3@yandex.ru>
2024-06-27 00:54:58 +0300

    [feat]: add FizzBuzz & Factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Let's calculate factorial! Enter a valid number: ");
+                bool isInt = int.TryParse(Console.ReadLine(), out int number);
+                if (isInt)
+                {
+                    int factorial = Factorial(number);
+                    Console.WriteLine($"Factorial {number} is {factorial:N0}");
+                }
+                else
+                {
+                    Console.WriteLine("Can't parse a number :C");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        // Factorial 5 = 5*4*3*2*1 = 120
+        // Factorial(3) = 3 * Factorial(2)
+        // Factorial(2) = 2 * Factorial(1)
+        // Factorial(1) = 1
+
+        static int Factorial(int number)
+        {
+            checked
+            {
+                return number < 2 ? 1 : number * Factorial(number - 1);
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool isFizz = i % 3 == 0;
+                bool isBuzz = i % 5 == 0;
+                Console.Write(
+                    $"{(isFizz ? "Fizz" : String.Empty)}" +
+                    $"{(isBuzz ? "Buzz" : String.Empty)}" +
+                    $"{(!(isFizz || isBuzz) ? i : String.Empty)}" +
+                    $"{(i < 100 ? ", " : String.Empty)}"
+                );
+            }
+        }
+    }
+}
