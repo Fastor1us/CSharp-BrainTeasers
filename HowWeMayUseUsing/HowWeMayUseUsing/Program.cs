285a434ac1bb86d6c506e77b05e6267c9d30139e
Oleg Kiryushin <fewgwer3@yandex.ru>
2025-02-04 00:50:03 +0300

    add new sln HowWeMayUseUsing

---
+++ HowWeMayUseUsing/HowWeMayUseUsing/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace HowWeMayUseUsing;
+
+readonly struct ConsoleForegroundContext : IDisposable
+{
+    private readonly ConsoleColor _previousColor;
+
+    public ConsoleForegroundContext(ConsoleColor color)
+    {
+        _previousColor = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+    }
+
+    public void Dispose()
+    {
+        Console.ForegroundColor = _previousColor;
+    }
+}
+
+internal class Program
+{
+    static ConsoleForegroundContext SwitchForegroundConsoleColor(ConsoleColor color)
+    {
+        return new ConsoleForegroundContext(color);
+    }
+
+    static void Main(string[] args)
+    {
+        using (SwitchForegroundConsoleColor(ConsoleColor.Red))
+        {
+            Console.WriteLine("Red");
+            using (SwitchForegroundConsoleColor(ConsoleColor.Green))
+            {
+                Console.WriteLine("Green");
+            }
+            Console.WriteLine("Red");
+        }
+        Console.WriteLine("White");
+    }
+}
