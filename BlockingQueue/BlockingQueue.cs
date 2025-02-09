9c7f2069e0d670af47c9c8b4bfc192f61fbfa17a
Oleg Kiryushin <fewgwer3@yandex.ru>
2025-02-09 19:56:57 +0300

    add sln BlockingQueue

---
+++ BlockingQueue/BlockingQueue.cs
@@ -0,0 +1,41 @@
+﻿namespace BlockingQueue;
+
+class BlockingQueue<T>
+{
+    private readonly object _lock = new();
+    private readonly Queue<T> _queue = new();
+
+    public void Enqueue(T value)
+    {
+        Monitor.Enter(_lock);
+        try
+        {
+            _queue.Enqueue(value);
+            if (_queue.Count == 1)
+            {
+                Monitor.Pulse(_lock);
+            }
+        }
+        finally
+        {
+            Monitor.Exit(_lock);
+        }
+    }
+
+    public T Dequeue()
+    {
+        Monitor.Enter(_lock);
+        try
+        {
+            while (_queue.Count == 0)
+            {
+                Monitor.Wait(_lock);
+            }
+            return _queue.Dequeue();
+        }
+        finally
+        {
+            Monitor.Exit(_lock);
+        }
+    }
+}
---
+++ BlockingQueue/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Concurrent;
+
+namespace BlockingQueue;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var q = new BlockingQueue<Action>();
+
+        var t = new Thread(ProcessingThread)
+        {
+            IsBackground = true,
+        };
+        t.Start(q);
+
+        Thread.Sleep(15000);
+
+        q.Enqueue(() =>
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Action completed");
+        });
+
+        Console.WriteLine("Main thread is free");
+        Console.ReadKey(true);
+
+        //ThreadPool.QueueUserWorkItem(_ =>
+        //{
+        //    Thread.Sleep(3000);
+        //    Console.WriteLine("Action completed");
+        //});
+
+        //ConcurrentQueue<string> q2 = new();
+        //if (!q2.TryDequeue(out var item)) { }
+    }
+
+    private static void ProcessingThread(object? obj)
+    {
+        if (obj is not BlockingQueue<Action> q) throw new Exception();
+
+        while (true)
+        {
+            var action = q.Dequeue();
+            action();
+        }
+    }
+}
