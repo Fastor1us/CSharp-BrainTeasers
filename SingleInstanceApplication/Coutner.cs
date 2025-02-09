5ab275ef996f6d7754244ff2f3eb84c8135ba49a
Oleg Kiryushin <fewgwer3@yandex.ru>
2025-02-09 19:58:29 +0300

    add sln SingleInstanceApplication

---
+++ SingleInstanceApplication/Coutner.cs
@@ -0,0 +1,31 @@
+﻿namespace SingleInstanceApplication;
+
+delegate void Printer(string msg);
+
+internal class Counter
+{
+    public event Printer? Printers;
+
+    public int Count { get; private set; }
+
+    public Counter IncreaseByValue(int value)
+    {
+        Count += value;
+        Printers?.Invoke($"Counter incresed by {value}, count is {Count}");
+        return this;
+    }
+
+    public Counter Increase()
+    {
+        Count += 1;
+        Printers?.Invoke($"Count increased, count is {Count}");
+        return this;
+    }
+
+    public Counter Decrease()
+    {
+        Count -= 1;
+        Printers?.Invoke($"Count decreased, count is {Count}");
+        return this;
+    }
+}
---
+++ SingleInstanceApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace SingleInstanceApplication;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // Single Instance Application
+        using (new Mutex(true, @"Global\M", out var createdNew))
+        {
+            if (!createdNew) return;
+
+            var counter = new Counter();
+
+            counter.Printers += PrintToConsole;
+            counter
+                .Increase()
+                .Increase()
+                .Decrease()
+                .IncreaseByValue(10);
+
+            Console.ReadKey();
+        }
+    }
+
+    private static void PrintToConsole(string msg)
+    {
+        Console.WriteLine(msg);
+    }
+}
+
